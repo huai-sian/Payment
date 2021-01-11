b5206760bbadfe05bf2c49d4637c38d201f6b2e2
huaisianhuang <bpud50122@gmail.com>
2021-01-11 09:43:45 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,50 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '*',
+    redirect: '/order/check'
+  },
+  {
+    path: '/order',
+    name: 'Order',
+    component: () => import('../views/Orders.vue'),
+    redirect: '/order/check',
+    children: [
+      {
+        path: 'check',
+        name: 'Orderlist',
+        component: () => import('../components/Orderlist.vue')
+      },
+      {
+        path: 'orderconvenient',
+        name: 'Orderconvenient',
+        component: () => import('../components/orderConvenient.vue')
+      },
+      {
+        path: 'orderatm',
+        name: 'Orderatm',
+        component: () => import('../components/atm.vue')
+      },
+      {
+        path: 'paycomplete',
+        name: 'PayComplete',
+        component: () => import('../components/PayComplete.vue')
+      },
+      {
+        path: 'ordercard',
+        name: 'Ordercard',
+        component: () => import('../components/orderCard.vue')
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
